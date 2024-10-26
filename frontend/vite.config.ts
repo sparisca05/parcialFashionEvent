f6ddc3a8bd96adb31b20a9150facec2b62eceae5
sparisca05 <simon.parisca@eia.edu.co>
2024-10-25 20:40:45 -0500

    docker melo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Esto permite que Vite escuche en todas las interfaces de red
+    port: 5173
+  }
 })
