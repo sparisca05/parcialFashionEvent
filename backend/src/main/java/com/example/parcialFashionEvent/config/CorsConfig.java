14493953bd8c758b6f5f24f69f4eb70b99ddabcc
sparisca05 <simon.parisca@eia.edu.co>
2024-10-25 20:57:03 -0500

    cambiar dominios que puedan acceder

--- backend/src/main/java/com/example/parcialFashionEvent/config/CorsConfig.java
+++ backend/src/main/java/com/example/parcialFashionEvent/config/CorsConfig.java
@@ -1,9 +1,7 @@
 package com.example.parcialFashionEvent.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -12,13 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Autowired
-    Environment env;
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin(env.getProperty("FRONT_URL"));
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
 
